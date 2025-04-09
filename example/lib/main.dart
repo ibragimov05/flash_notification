a2e309d23ba788f4b59c82a4d0b457213c41ed39
fazliddin <ibragimov.asus15@gmail.com>
2025-04-09 09:11:08 +0500

    example edited

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,25 +1,21 @@
 import 'package:flash_notification/flash_notification.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple)),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  Widget build(BuildContext context) => MaterialApp(
+    title: 'Flutter Demo',
+    theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple)),
+    home: const MyHomePage(title: 'Flash Notification Example'),
+  );
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({required this.title, super.key});
 
   final String title;
 
@@ -28,30 +24,35 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final int _counter = 0;
+  /// Shows a flash notification.
+  void _showFlashNotification() => FlashNotification.show(context: context, message: 'Hello, world!');
 
-  void _incrementCounter() {
-    FlashNotification.show(context: context, message: 'Hello, world!', icon: Icon(Icons.delete));
-  }
+  /// Shows a flash notification with an icon.
+  void _showFlashNotificationWithIcon() => FlashNotification.show(
+    context: context,
+    message: 'Hello, world!',
+    icon: const Icon(Icons.notification_add, color: Colors.white),
+  );
+
+  /// Shows a flash notification with a long duration.
+  void _showFlashNotificationWithLongDuration() =>
+      FlashNotification.show(context: context, message: 'Hello, world!', duration: const Duration(seconds: 10));
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(backgroundColor: Theme.of(context).colorScheme.inversePrimary, title: Text(widget.title)),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text('$_counter', style: Theme.of(context).textTheme.headlineMedium),
-          ],
-        ),
+  Widget build(BuildContext context) => Scaffold(
+    appBar: AppBar(backgroundColor: Theme.of(context).colorScheme.inversePrimary, title: Text(widget.title)),
+    body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          FilledButton(onPressed: _showFlashNotification, child: Text('Flash notification')),
+          FilledButton(onPressed: _showFlashNotificationWithIcon, child: Text('Flash notification with icon')),
+          FilledButton(
+            onPressed: _showFlashNotificationWithLongDuration,
+            child: Text('Flash notification with long duration'),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
+    ),
+  );
 }
